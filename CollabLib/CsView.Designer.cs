5a719d3f9f504d8e7f9551711cdf244e7fe55e43
高萩 俊行 <tosiyuki@hh.iij4u.or.jp>
2018-11-13 15:02:50 +0900

    プロジェクト ファイルを追加します。

---
+++ CollabLib/CsView.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CollabLib
+{
+	partial class CsView
+	{
+		/// <summary> 
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing && (components != null) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region コンポーネント デザイナーで生成されたコード
+
+		/// <summary> 
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(255, 71);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "C#のコントロール。特に操作はしない。";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// CsView
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.Controls.Add(this.label1);
+			this.Name = "CsView";
+			this.Size = new System.Drawing.Size(255, 71);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ CollabWpf/WrapWpf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabWpf
+{
+	public class WrapWpf
+	{
+		public static int CallWpfDlg()
+		{
+			var dlg = new WpfDlg();
+			dlg.ShowDialog();
+			return dlg.ButtonValue;
+		}
+	}
+}
