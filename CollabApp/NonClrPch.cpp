108a6bcbb522352d65d042d55170a89493feee6c
高萩 俊行 <tosiyuki@hh.iij4u.or.jp>
2018-11-21 15:53:18 +0900

    .NET 混在が全くない CMainFrame/CCollabAppDoc は Native でビルドするように変更

---
+++ CollabApp/NonClrPch.cpp
@@ -0,0 +1,1 @@
+#include "NonClrPch.h"
---
+++ CollabApp/NonClrPch.h
@@ -0,0 +1,20 @@
+
+// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
+// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
+// を記述します。
+
+#pragma once
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // Windows ヘッダーから使用されていない部分を除外します。
+#endif
+
+#include "targetver.h"
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // 一部の CString コンストラクターは明示的です。
+
+// 一般的で無視しても安全な MFC の警告メッセージの一部の非表示を解除します。
+#define _AFX_ALL_WARNINGS
+
+#include <afxwin.h>         // MFC のコアおよび標準コンポーネント
+#include <afxext.h>         // MFC の拡張部分
